57676a8171f3449d5a6be78646bb623fa69115c2
fksanfkdnf <118088068+fksanfkdnf@users.noreply.github.com>
2022-11-14 08:11:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐 Hê sờ lô hơ sờ ly ly 🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, phải trả lời thật đó nha lòng nha!!!`,
+    btnIntro: 'OK',
+    title: 'Em thích Anh phải không phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
+    btnYes: 'Có, em thật sự thích anh 💖💖💖',
+    btnNo: '💔 Anh mới bú đá à ??? 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó Ahihihihi 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: 'Cho em xem một bất ngờ nè :33 ',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://fksanfkdnf.github.io/trai-tim/'
+}
